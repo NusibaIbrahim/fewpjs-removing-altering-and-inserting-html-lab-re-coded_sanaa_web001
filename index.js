95c998d4e9d38e8e904a05bd6272785a2dfb5c0a
Nusiba Ibrahim <ainusiba@gmail.com>
2020-07-19 18:01:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Write your code here!
 main.remove();
 var newHeader = document.createElement("H1");
-
-let newHeader = document.querySelector("H1#victory");
-element.innerHTML = 'Hello, DOM!';
+newHeader = 
