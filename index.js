b86b2085a38c31e28fa4b4e45543ca71b7ed3731
Nusiba Ibrahim <ainusiba@gmail.com>
2020-07-19 17:44:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ main.remove();
 
 let newHeader = document.createElement('h1#victory');
 document.body.appendChild(newHeader);
-let newHeader = document.getElementById("h1#victory");
+newHeader = document.getElementById("h1#victory");
 header.innerHTML ="YOUR-NAME is the champion"
